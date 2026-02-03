209f014883febfbb0cae521c5bd28931df6aa656
Sanjide <sanjidmsnjdp@gmail.com>
2026-02-03 23:12:05 +0530

    Delete lib directory

--- lib/config/theme.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-ThemeData buildAppTheme() {
-  return ThemeData(
-    colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
-    useMaterial3: true,
-  );
-}
