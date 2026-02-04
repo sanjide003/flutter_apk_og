ecc3149b7c02910d883f5141eae62297f78724cd
Sanjide <sanjidmsnjdp@gmail.com>
2026-02-04 06:06:18 -0500

    Update firebase_options.dart

--- lib/config/firebase_options.dart
+++ lib/config/firebase_options.dart
@@ -1,4 +1,3 @@
-// ഫയർബേസ് കോൺഫിഗറേഷൻ ഫയൽ
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
 
@@ -12,21 +11,6 @@ class DefaultFirebaseOptions {
         return android;
       case TargetPlatform.iOS:
         return ios;
-      case TargetPlatform.macOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for macos - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      case TargetPlatform.windows:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for windows - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      case TargetPlatform.linux:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for linux - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
       default:
         throw UnsupportedError(
           'DefaultFirebaseOptions are not supported for this platform.',
@@ -34,7 +18,7 @@ class DefaultFirebaseOptions {
     }
   }
 
-  // WEB CONFIGURATION (താങ്കൾ നൽകിയ കീ ഇവിടെ ചേർക്കുന്നു)
+  // WEB CONFIGURATION
   static const FirebaseOptions web = FirebaseOptions(
     apiKey: "AIzaSyDcxrvgGTcc-w2LgY1Hc1Jy6EPs7fJGFJs",
     authDomain: "fee-apk-1.firebaseapp.com",
@@ -45,10 +29,10 @@ class DefaultFirebaseOptions {
     measurementId: "G-EFWCZCJRSL",
   );
 
-  // ANDROID CONFIGURATION (Web key തന്നെ താൽക്കാലികമായി ഉപയോഗിക്കാം അല്ലെങ്കിൽ Android-ന് വേണ്ടിയുള്ളത് ഇവിടെ നൽകാം)
+  // ANDROID CONFIGURATION (Updated with Web Keys for testing)
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: "AIzaSyDcxrvgGTcc-w2LgY1Hc1Jy6EPs7fJGFJs", // Android API Key മാറ്റേണ്ടി വരും
-    appId: "1:178081719111:android:xxxxxx", // Android App ID ഇവിടെ നൽകണം
+    apiKey: "AIzaSyDcxrvgGTcc-w2LgY1Hc1Jy6EPs7fJGFJs",
+    appId: "1:178081719111:web:44e95480cff6df2a5a2262", // Web App ID തന്നെ ഉപയോഗിക്കുന്നു
     messagingSenderId: "178081719111",
     projectId: "fee-apk-1",
     storageBucket: "fee-apk-1.firebasestorage.app",
@@ -56,11 +40,10 @@ class DefaultFirebaseOptions {
 
   // iOS CONFIGURATION
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: "AIzaSyDcxrvgGTcc-w2LgY1Hc1Jy6EPs7fJGFJs", // iOS API Key
-    appId: "1:178081719111:ios:xxxxxx", // iOS App ID
+    apiKey: "AIzaSyDcxrvgGTcc-w2LgY1Hc1Jy6EPs7fJGFJs",
+    appId: "1:178081719111:web:44e95480cff6df2a5a2262",
     messagingSenderId: "178081719111",
     projectId: "fee-apk-1",
     storageBucket: "fee-apk-1.firebasestorage.app",
-    iosBundleId: "com.example.institutionOs",
   );
 }
